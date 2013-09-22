7ab87e15dcea7b3fe9257748f85e5c00571abf8a
Daniel Choi <dhchoi@gmail.com>
2013-09-21 21:58:29 -0400

    basic parsers

--- src/Core.hs
+++ src/Core.hs
@@ -1,18 +1,32 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Core where
-import Data.Attoparsec.Char8
+import Data.Attoparsec.Text
 import Control.Applicative
 import Data.Text (Text)
+import Data.Char
 import qualified Data.Text as T
 
 type LatLng = (Double, Double)
 
-data Event = Rename Text 
-           | Center LatLng
-           | Chat Text
+data Event = Rename Text Text
+           | Locate Text LatLng
+           | Chat Text Text
            deriving (Show, Eq)
 
-
 eventParser :: Parser Event
-eventParser = undefined
+eventParser = parseRename <|> parseLocate <|> parseChat
 
+parseName = takeWhile1 isAlphaNum
+
+parseRename = Rename <$> (string "rename" >> char ' ' *> parseName) <*> (char ' ' *> parseName) 
+
+parseLocate = do
+  string "locate" 
+  Locate <$> (char ' ' *> parseName) <*> ((,) <$> (char ' ' *> double) <*> (char ' ' *> double))
+
+parseChat = 
+  Chat <$> (string "chat" >> char ' ' *> parseName <* char ' ') <*> takeTill (== '\n') 
+
+-- for development
+test :: String -> Either String Event
+test s = parseOnly eventParser (T.pack s)
