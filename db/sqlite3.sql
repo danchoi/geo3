7bd021ebe6a99bce2df52805cb11517e5cab10e9
Daniel Choi <dhchoi@gmail.com>
2013-09-26 10:26:29 -0400

    revise schema
    Events is overboard. Replace with posts.
    
    Sessions is better tailored to what the UI is actually representing.

--- db/sqlite3.sql
+++ db/sqlite3.sql
@@ -1,22 +1,21 @@
-
-create table events (
-  event_user text,
-  event_log text,
-  event_created_at datetime default current_timestamp
-);
-
-create table users (
-  user_name text UNIQUE,
-  user_lat real,
-  user_lng real,
-  user_zoom num
-);
-
 create table posts (
-  user_name text,
+  session integer,
+  post_author text,
   post_lat real,
   post_lng real,
   post_zoom num,
-  post_created_t datetime default current_timestamp
+  post_text text,
+  post_created_at datetime default current_timestamp
+);
+
+create table sessions (
+  session integer primary key,
+  session_nickname text,
+  session_lat real,
+  session_lng real,
+  session_zoom num,
+  session_created_at datetime default current_timestamp,
+  session_updated_at datetime default current_timestamp,
+  session_security_hash text UNIQUE
 );
 
